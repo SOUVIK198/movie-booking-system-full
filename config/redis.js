9524a7ce32077cb88402bb2547b76e04acd75ea6
souvik singha <singhasouvik198@gmail.com>
2026-07-30 21:09:47 +0530

    Add Redis caching support

---
+++ config/redis.js
@@ -0,0 +1,19 @@
+const { createClient } = require("redis");
+
+const redisClient = createClient({
+    url: process.env.REDIS_URL,
+});
+
+redisClient.on("connect", () => {
+    console.log("Redis Connected");
+});
+
+redisClient.on("error", (err) => {
+    console.log("Redis Error:", err.message);
+});
+
+(async () => {
+    await redisClient.connect();
+})();
+
+module.exports = redisClient;
